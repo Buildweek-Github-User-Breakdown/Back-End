1dc2645d6dfcf4b3ed3bbdc0ca262ea5063fd5a0
ren.meta@gmail.com <ren.meta@gmail.com>
2019-09-26 11:53:40 -0400

    User Repo and User Service

---
+++ githubuser/src/main/java/com/lambdaschool/githubuser/services/UserNotesService.java
@@ -0,0 +1,18 @@
+package com.lambdaschool.githubuser.services;
+
+import com.lambdaschool.githubuser.models.UserNotes;
+
+import java.util.List;
+
+public interface UserNotesService {
+
+    List<UserNotes> findAll();
+
+    UserNotes findUsernotesById(long id);
+
+    List<UserNotes> findByUserName(String username);
+
+    void delete(long id);
+
+    UserNotes save(UserNotes userNotes);
+}
